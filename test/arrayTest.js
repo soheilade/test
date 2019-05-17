23dcc6e3ad83e2c3de161013bb38ef99248f6d2e
Michael W <developerwakeling@gmail.com>
2019-05-16 22:23:33 -0400

    Failing Test

--- test/arrayTest.js
+++ test/arrayTest.js
@@ -5,7 +5,7 @@ describe('Array', function(){
         it('Should return -1 if the value is not in the array or a positive number (index) if it does exist', function(){
             assert.equal([1,2,3].indexOf(4), -1);
             assert.equal([1,2,3].indexOf(1), 0);
-            assert.equal([1,2,3].indexOf(3), 2);
+            assert.equal([1,2,3].indexOf(3), 3);
         });
     });
 });
